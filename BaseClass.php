de8f6310cca65c0d1223a972e1aaafc6ea7b0cc1
regenadaster <594735553@qq.com>
2014-06-04 15:35:24 +0800

    add one word to editor

--- BaseClass.php
+++ BaseClass.php
@@ -22,4 +22,39 @@
   	  return $this->PassWord;
   	}
   }
+  class splider{
+  	var $url;
+  	var $content;
+  	function setURL($_url){
+  	  $this->url=$_url;
+  	}
+  	function getContent(){
+  	  $content=file_get_contents($this->url);
+  	  return $content;
+  	}
+  }
+  class OneDayOneWord{
+  	var $theDate;
+  	var $content;
+  	var $author;
+    var $smallsplider;
+    var $word;
+    function __construct(){
+      $this->smallsplider=new splider();
+      $this->smallsplider->setURL("http://xue.youdao.com/w");
+      $this->content=$this->smallsplider->getContent();  
+      $contentArr=preg_split('/<p class="sen">/',$this->content);
+      $word=preg_split('/</',$contentArr[1]);
+      //print_r($word);
+      $this->word=$word[0];
+      //$this->word=$wordAndAuthor[0];
+      //$this->author=$wordAndAuthor[1];
+    }
+    function getWord(){
+      return $this->word;
+    }
+    function getAuthor(){
+      return $this->author;
+    }
+  }
 ?>
--- server.php
+++ server.php
@@ -2,9 +2,16 @@
   include "BaseClass.php";
   $setting=new BaseSetting();
   $setting->setBackGround("gray");
-  //if($_POST["query"]=="background"){
+  if($_GET["query"]=="background"){
     $answer=$setting->getBackGround();
     $data=array("background"=>$answer);
     echo json_encode($data);
-//  }
+  }
+  $oneword=new OneDayOneWord();
+  $word=$oneword->getWord();
+  $author=$oneword->getAuthor();
+  if($_GET["query"]=="word"){
+  	$data=array("word"=>$word);
+  	echo json_encode($data);
+  }
 ?>
